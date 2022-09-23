dfe063b4fe68c879d31d59e03dae1e8e34f845e7
Jack Holmes <jdh97@comcast.net>
2022-09-22 21:09:57 -0400

    commit tree bddff1c7c09c5d54685cce91679836275304be5e

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+const employee = {
+    name : "Jack",
+    streetAddress : {
+        street : {
+            line1 : "8 East 30 Street",
+            line2 : "Apt 20"
+        },
+        city : "Brooklyn",
+        state : "New York",
+        zipCode : "10012"
+    }
+
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployee = { ...employee };
+    updatedEmployee[key] = value;
+    return updatedEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const pastEmployee = { ...employee };
+    delete pastEmployee[key];
+    return pastEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
